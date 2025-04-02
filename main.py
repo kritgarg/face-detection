2c6bb3c6c33dac955d54aaa515d12844a9aef699
kritgarg <kritg0160@gmail.com>
2025-04-02 13:58:55 +0530

    ready for deploy

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import cv2
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+st.title("Live Webcam Stream")
 
 video_capture = cv2.VideoCapture(0)
+frame_placeholder = st.empty()
+
 while True:
-    _, frame = video_capture.read()
-    cv2.imshow('Video', frame)
-    if cv2.waitKey(1) == ord('q'):
+    ret, frame = video_capture.read()
+    if not ret:
+        st.write("Failed to capture video")
         break
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_placeholder.image(frame, channels="RGB")
+
 video_capture.release()
